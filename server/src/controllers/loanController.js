6c8d98cd8ade35432d77f27c1071bf93f4f26445
Tanishq Aswar <tanishqaswar2005@gmail.com>
2026-04-28 01:05:42 +0530

    Fix: resolve floating point dust preventing loans from closing

--- server/src/controllers/loanController.js
+++ server/src/controllers/loanController.js
@@ -277,7 +277,8 @@ exports.recordPayment = async (req, res) => {
     loan.payments.push({ utrNumber, amount, date, recordedBy: req.user._id });
     loan.outstandingBalance = Math.round((loan.outstandingBalance - amount) * 100) / 100;
 
-    if (loan.outstandingBalance <= 0) {
+    // Handle fractional dust (< 1 Rupee) that gets hidden by UI formatting
+    if (loan.outstandingBalance < 1) {
       loan.status = 'CLOSED';
       loan.outstandingBalance = 0;
     }
